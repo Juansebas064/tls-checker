82c171266974120151c528038def5e8f6bef59ef
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2026-02-05 02:11:25 -0500

    Add each section's keyboard shortcuts

--- internal/ui/keyboard.go
+++ internal/ui/keyboard.go
@@ -6,10 +6,14 @@ func (app *Application) setKeyboardShortcuts() {
 	app.tui.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 
 		switch event.Rune() {
-		case rune(tcell.KeyCtrlS):
-			app.tui.SetFocus(app.searchBarSection)
 		case rune(tcell.KeyCtrlH):
 			app.tui.SetFocus(app.hostsSection)
+		case rune(tcell.KeyCtrlE):
+			app.tui.SetFocus(app.endpointsSection)
+		case rune(tcell.KeyCtrlD):
+			app.tui.SetFocus(app.detailsSection)
+		case rune(tcell.KeyCtrlS):
+			app.tui.SetFocus(app.searchBarSection)
 		// Exit the app
 		case rune(tcell.KeyCtrlQ):
 			app.tui.Stop()
