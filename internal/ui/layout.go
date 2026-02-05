2165e96a8ad3b451b7668fb13e3ee9ea976998bb
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2026-02-05 02:13:04 -0500

    Set frames for each section for styling headers and footers

--- internal/ui/layout.go
+++ internal/ui/layout.go
@@ -6,32 +6,49 @@ import (
 )
 
 func (app *Application) buildLayout() {
+	// Frames for headers and shortcuts guide
 	hostsFrame := tview.NewFrame(app.hostsSection)
-	hostsFrame.AddText("Visited Hosts (Ctrl + h)", true, tview.AlignLeft, tcell.Color63).
-		// AddText("Ctrl + h", false, tview.AlignLeft, tcell.Color110).
-		SetBorders(0, 0, 0, 0, 1, 1).
+	hostsFrame.AddText("Hosts [Ctrl + h]", true, tview.AlignLeft, tcell.Color63).
+		SetBorders(0, 0, 1, 0, 1, 1).
+		SetBorder(true)
+
+	endpointsFrame := tview.NewFrame(app.endpointsSection)
+	endpointsFrame.AddText("Endpoints [Ctrl + e]", true, tview.AlignLeft, tcell.Color63).
+		SetBorders(0, 0, 1, 0, 1, 1).
+		SetBorder(true)
+
+	detailsFrame := tview.NewFrame(app.detailsSection)
+	detailsFrame.AddText("Details [Ctrl + d]", true, tview.AlignCenter, tcell.Color63).
+		SetBorders(0, 0, 1, 0, 1, 1).
+		SetBorder(true)
+
+	searchBarFrame := tview.NewFrame(app.searchBarSection)
+	searchBarFrame.AddText("Search [Ctrl + s]", true, tview.AlignCenter, tcell.Color63).
+		SetBorders(0, 0, 1, 0, 1, 1).
 		SetBorder(true)
 
 	// Build layout
 	hostsAndEndpointLayout := tview.NewFlex().
 		SetDirection(tview.FlexColumnCSS).
 		AddItem(hostsFrame, 0, 1, false).
-		AddItem(app.endpointsSection, 0, 1, false)
+		AddItem(endpointsFrame, 0, 1, false)
 
 	firstColumnAndDetailsLayout := tview.NewFlex().
 		SetDirection(tview.FlexRowCSS).
 		AddItem(hostsAndEndpointLayout, 0, 1, false).
-		AddItem(app.detailsSection, 0, 3, false)
+		AddItem(detailsFrame, 0, 3, false)
 
 	infoAndSearchHostLayout := tview.NewFlex().
 		SetDirection(tview.FlexColumnCSS).
-		AddItem(firstColumnAndDetailsLayout, 0, 5, false).
-		AddItem(app.searchBarSection, 0, 1, true)
+		AddItem(firstColumnAndDetailsLayout, 0, 10, false).
+		AddItem(searchBarFrame, 0, 2, true).
+		AddItem(app.messagesSection.SetTextAlign(tview.AlignCenter), 0, 1, false)
 
-	mainFrame := tview.NewFrame(infoAndSearchHostLayout).SetBorders(0, 0, 0, 0, 0, 0)
+	// Set the final layout
+	mainFrame := tview.NewFrame(infoAndSearchHostLayout).SetBorders(1, 0, 0, 0, 0, 0)
 	mainFrame.AddText("TLS Checker - SSL Labs Api v2", true, tview.AlignCenter, tcell.Color63)
 
-	// Set up layout in main app
+	// Set up final layout in main app
 	app.pages = tview.NewPages().AddPage("main", mainFrame, true, true)
 	app.tui = tview.NewApplication().SetRoot(app.pages, true)
 
