ec3c451373cb5665e6dc8640c5239a647d16e28c
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2026-02-09 19:34:06 -0500

    Refactor searchHost to handle checking status every 10 seconds

--- internal/ui/actions.go
+++ internal/ui/actions.go
@@ -5,44 +5,46 @@ import (
 	"fmt"
 	"time"
 	"tls-checker/internal/api"
-	"tls-checker/internal/model"
+	// "tls-checker/internal/model"
 )
 
-// Call api.analyzeHost(host) with the given host name and add it to []visitedHosts if it was not present. Call app.hostChanged(host) at the end.
+// Call api.analyzeHost(analyzeHostQuery) with the given host name and add it to []visitedHosts if it was not present. Call app.hostChanged(host) at the end.
 func (app *Application) searchHost(host string) {
 	go func() {
 		if _, exists := app.visitedHosts[host]; !exists {
-			var (
-				res *model.Host
-				err error
-				i   int = 0
-			)
-			for {
-				if res != nil {
-					res, err = api.AnalyzeHost(app.getAnalyzeHostQuery(true))	// Check status of running req
-					app.showMessage(fmt.Sprintf("Retrying for %d time, status: %s", i, res.Status), "text")
-				} else {
-					res, err = api.AnalyzeHost(app.getAnalyzeHostQuery(false))
-					app.showMessage("Starting API request", "text")
+			// Make the request
+			app.showMessage("Starting API request", "text")
+			res, err := api.AnalyzeHost(app.getAnalyzeHostQuery(false))
+
+			if err != nil {
+				app.showMessage(err.Error(), "error")
+				return
+			}
+
+			// Keep checking until status is ready
+			for i := 1; ; i++ {
+				if res.Status == "READY" {
+					break
 				}
+
+				time.Sleep(10 * time.Second)
+
+				res, err = api.AnalyzeHost(app.getAnalyzeHostQuery(true))
+				app.showMessage(fmt.Sprintf("Retrying for %d time, status: %s", i, res.Status), "text")
 
 				if err != nil {
 					app.showMessage(err.Error(), "error")
-					break
-				} else {
-					if res.Status != "READY" {
-						time.Sleep(5 * time.Second)
-					} else {
-						// Add host to visited hosts
-						app.visitedHosts[host] = res
-						app.hostsSection.AddItem(host, "", 0, nil)
-						app.showMessage("API request completed", "text")
-						app.hostChanged(host)
-						break
-					}
+					return
 				}
-				i++
 			}
+
+			// Add host to visited hosts
+			app.visitedHosts[host] = res
+			app.hostsSection.AddItem(host, "", 0, nil)
+			app.showMessage("API request completed", "text")
+			app.hostChanged(host)
+		} else {
+			app.hostChanged(host)
 		}
 	}()
 }
@@ -50,6 +52,11 @@ func (app *Application) searchHost(host string) {
 // Change the contents of endpointsSection and detailsSection when adding/navigating to another host in the list
 func (app *Application) hostChanged(host string) {
 	app.tui.SetFocus(app.hostsSection)
+
+	// TODO: If host isn't already in focus
+
+	// if hostAlreadyOnFocus := app.hostsSection.
+
 	app.queueUpdateDraw(func() {
 
 		// Set host on focus
