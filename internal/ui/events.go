1b5f1a75e7174ded3ee3cdc8ffba8bd284bac9e7
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2026-02-10 13:54:01 -0500

    Refactor events binding to events.go

---
+++ internal/ui/events.go
@@ -0,0 +1,35 @@
+package ui
+
+// Sets events when specific functions are triggered in the components
+func (app *Application) setEvents() {
+	// Hosts
+	app.hostsSection.SetChangedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
+		app.hostChanged(index)
+	})
+	app.hostsSection.SetSelectedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
+		app.tui.SetFocus(app.detailsSection)
+	})
+	//Endpoints
+	app.endpointsSection.SetChangedFunc(func(index int, mainText, secondaryText string, shortcut rune) {
+		app.endpointChanged(index)
+	})
+	app.endpointsSection.SetSelectedFunc(func(index int, mainText, secondaryText string, shortcut rune) {
+		app.tui.SetFocus(app.detailsSection)
+	})
+	// Search
+	app.searchSection.GetButton(0).SetSelectedFunc(func() {
+		app.searchHost(app.hostField.GetText())
+	})
+	app.startNewCheck.SetChangedFunc(func(isChecked bool) {
+		if isChecked {
+			app.fromCacheCheck.SetChecked(false)
+			app.maxAgeField.SetDisabled(true)
+		}
+	})
+	app.fromCacheCheck.SetChangedFunc(func(isChecked bool) {
+		if isChecked {
+			app.startNewCheck.SetChecked(false)
+			app.maxAgeField.SetDisabled(false)
+		}
+	})
+}
